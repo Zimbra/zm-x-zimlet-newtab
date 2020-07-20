ccbc7e78a3b99455e30bff9cd622dbade5c12233
Yashraj Chavda <yashrajpchavda@gmail.com>
2020-07-20 19:44:03 +0530

    master: removed unused import

--- src/components/app/index.js
+++ src/components/app/index.js
@@ -1,6 +1,5 @@
 import { createElement, Component } from 'preact';
 import { Button } from '@zimbra-client/blocks';
-import style from './style';
 // Can also use shimmed decorators like graphql or withText.
 // Or, utils, like callWtih. Refer to zm-x-web, zimbraManager/shims.js
 // More shims can be added here if necessary; also requires an update to zimlet-cli
